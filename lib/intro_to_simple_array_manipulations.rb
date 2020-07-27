875020ac9ca8eaa87fed7eeada2e62e0fde2cf4c
Christian P Andrews <cpandrews91@gmail.com>
2020-07-27 01:12:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,30 +1,30 @@
 #using_push
 def using_push
-Array.push(4)
+array.push(4)
 end
 
 #using_unshift
 def using_unshift
-Array.(3)
-Array.unshift(0)
+array.(3)
+array.unshift(0)
 end
 
 #using_pop
 def using_pop
-  Array(3).pop
+  array(3).pop
 end
 
 #pop_with_args
 def pop_with_args
-  Array(3).pop(2)
+  array(3).pop(2)
 end
 
 #using_shift
 def using_shift
-  Array(3).shift
+  array(3).shift
 end
 
 #shift_with_args
 def shift_with_args
-  Array(3).shift(2)
+  array(3).shift(2)
 end
